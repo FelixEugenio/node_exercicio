87d8d18888cd59d8724bf7cd28f515841600e46b
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-20 23:05:15 +0000

    feat: delete task todo

--- server.js
+++ server.js
@@ -67,7 +67,20 @@ app.get('/to-do/:id', (req, res) => {
     if(!verifyIfUserAlreadyExists) {
         return res.status(404).json({error: 'User does not exists'});
     }
-    
+
+    const todo = toDo.filter((todo) => todo.user_id === id);
+    res.status(200).json(todo);
+});
+
+app.delete('/to-do/:id', (req, res) => {
+    const {id} = req.params;
+
+    const verifyIfUserAlreadyExists = users.find((user) => user.id === id)
+
+    if(!verifyIfUserAlreadyExists) {
+        return res.status(404).json({error: 'User does not exists'});
+    }
+
     const todo = toDo.filter((todo) => todo.user_id === id);
     res.status(200).json(todo);
 });
