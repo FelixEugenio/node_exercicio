43e4be31f62ffb0054e6247a52c26347678a458b
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-20 22:49:51 +0000

    feat: create task

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 const users = [];
+const toDo = [];
 
 app.get('/users', (req, res) => {
     res.send(users);
@@ -21,7 +22,7 @@ app.post('/users', (req, res) => {
     if(verifyIfUserAlreadyExists) {
         return res.status(400).json({error: 'User already exists'});
     }
-    
+
     const user = {
         id: uuidv4(),
         name,
@@ -31,6 +32,28 @@ app.post('/users', (req, res) => {
 
     users.push(user);
     res.status(201).json(user);
+});
+
+app.post('/to-do/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, description} = req.body;
+
+    const verifyIfUserAlreadyExists = users.find((user) => user.id === id)
+
+    if(!verifyIfUserAlreadyExists) {
+        return res.status(404).json({error: 'User does not exists'});
+    }
+
+    const todo ={
+        name,
+        description,
+        created_at: new Date(),
+        user_id: id
+    }
+
+    toDo.push(todo);
+    res.status(201).json(todo);
+
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
